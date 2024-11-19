61e68a12722153c6a7dba1ae68adef57735b559c
Elizaveta A <ahmatovaliza2006@gmail.com>
2024-11-19 14:37:50 +0300

    Исправлены ошибкив test_triangle.py

--- triangle.py
+++ triangle.py
@@ -3,7 +3,7 @@ import math
 
 def area(a, b, c):
     """Вычисляет площадь треугольника по формуле Герона."""
-    if a <= 0 or b <= 0 or c <= 0:
+    if a < 0 or b < 0 or c < 0:
         raise ValueError("Стороны треугольника должны быть положительными")
 
     if a + b <= c or a + c <= b or b + c <= a:
@@ -15,7 +15,7 @@ def area(a, b, c):
 
 def perimeter(a, b, c):
     """Вычисляет периметр треугольника."""
-    if a <= 0 or b <= 0 or c <= 0:
+    if a < 0 or b < 0 or c < 0:
         raise ValueError("Стороны треугольника должны быть положительными")
 
     return a + b + c
